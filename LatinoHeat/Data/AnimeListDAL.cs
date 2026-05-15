e739efbf73793fd5158770923b0f44fed6e4e81e
Deven Coreas <deven.coreas@dsdlabs.com>
2026-05-14 18:30:53 -0600

    Anime model, DAL, and DbContext made.

---
+++ LatinoHeat/Data/AnimeListDAL.cs
@@ -0,0 +1,33 @@
+﻿using LatinoHeat.Interface;
+using LatinoHeat.Models;
+
+namespace LatinoHeat.Data
+{
+	public class AnimeListDAL : IDataAccessLayer
+	{
+		public void CreateAnime(Anime anime)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<Anime> FilterAnime(string AnimeName)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<Anime> GetAnime()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Anime? GetAnime(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void UpdateUpdate(Anime anime)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
